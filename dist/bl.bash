8881a2d167e3fd9d87b620c6cd9d143696ea00ff
yuniel <yuniel.work@gmail.com>
2020-08-28 20:06:49 -0400

    export colors as readonly variables

--- dist/bl.bash
+++ dist/bl.bash
@@ -8,37 +8,24 @@ set -euEo pipefail
 
 ## Colors
 
-# shellcheck disable=SC2034
-declare -rx NOCOLOR='\033[0m'
-# shellcheck disable=SC2034
-declare -rx RED='\033[0;31m'
-# shellcheck disable=SC2034
-declare -rx GREEN='\033[0;32m'
-# shellcheck disable=SC2034
-declare -rx ORANGE='\033[0;33m'
-declare -rx BLUE='\033[0;34m'
-# shellcheck disable=SC2034
-declare -rx PURPLE='\033[0;35m'
-# shellcheck disable=SC2034
-declare -rx CYAN='\033[0;36m'
-# shellcheck disable=SC2034
-declare -rx LIGHTGRAY='\033[0;37m'
-# shellcheck disable=SC2034
-declare -rx DARKGRAY='\033[1;30m'
-# shellcheck disable=SC2034
-declare -rx LIGHTRED='\033[1;31m'
-# shellcheck disable=SC2034
-declare -rx LIGHTGREEN='\033[1;32m'
-# shellcheck disable=SC2034
-declare -rx YELLOW='\033[1;33m'
-# shellcheck disable=SC2034
-declare -rx LIGHTBLUE='\033[1;34m'
-# shellcheck disable=SC2034
-declare -rx LIGHTPURPLE='\033[1;35m'
-# shellcheck disable=SC2034
-declare -rx LIGHTCYAN='\033[1;36m'
-# shellcheck disable=SC2034
-declare -rx WHITE='\033[1;37m'
+export NOCOLOR='\033[0m'
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export ORANGE='\033[0;33m'
+export BLUE='\033[0;34m'
+export PURPLE='\033[0;35m'
+export CYAN='\033[0;36m'
+export LIGHTGRAY='\033[0;37m'
+export DARKGRAY='\033[1;30m'
+export LIGHTRED='\033[1;31m'
+export LIGHTGREEN='\033[1;32m'
+export YELLOW='\033[1;33m'
+export LIGHTBLUE='\033[1;34m'
+export LIGHTPURPLE='\033[1;35m'
+export LIGHTCYAN='\033[1;36m'
+export WHITE='\033[1;37m'
+readonly NOCOLOR RED GREEN ORANGE BLUE PURPLE CYAN LIGHTGRAY DARKGRAY LIGHTRED \
+         LIGHTGREEN YELLOW LIGHTBLUE LIGHTPURPLE LIGHTCYAN WHITE
 
 # Functions
 
